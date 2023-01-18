77f8b68c581c6b13fd80f0a33981020bbc0a74c8
Laurent <101107400+lolothi@users.noreply.github.com>
2023-01-18 09:27:28 +0100

    crudnotes test  accès db (#17)
    * creation repo + entity Note
    
    * creation methode dans repo note
    
    * maj
    
    * add test db notes

--- src/test/java/com/agilescrum/notes/NotesApplicationTests.java
+++ src/test/java/com/agilescrum/notes/NotesApplicationTests.java
@@ -1,13 +1,34 @@
 package com.agilescrum.notes;
 
+import com.agilescrum.notes.entities.Matiere;
+import com.agilescrum.notes.entities.Note;
+import com.agilescrum.notes.repositories.NoteRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class NotesApplicationTests {
 
+	@Autowired
+	private NoteRepository noteRepository;
+
+	private Matiere matiere = null;
+
+
+
 	@Test
-	void contextLoads() {
+	void shouldNotesInDB() {
+		System.out.println("hello");
+		List<Note> notes = (List<Note>) noteRepository.findAll();
+		System.out.println(notes);
+
+		final List<Note> note1 = noteRepository.findByMatiereIdAndUtilisateurId(1L, 3L);
+		System.out.println(note1);
+
+
 	}
 
 }
